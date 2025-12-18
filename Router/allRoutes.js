0e70f9b877a5ba6065ba31295f911bcca385676e
RakkyzDeveloppement <hugologer8867@gmail.com>
2025-12-18 21:37:56 +0100

    Ajout gestion compte

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,10 @@ export const allRoutes = [
     new Route("/index.html", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/carte", "La carte", "/pages/carte.html"),
+    new Route("/signin", "Connexion","/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Modification du mot de passe", "/pages/auth/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
