d1945d362a9042b241839b8f7fbe1dba4c7ca919
RakkyzDeveloppement <hugologer8867@gmail.com>
2025-12-22 20:32:50 +0100

    Ajour de la page vos reservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -9,6 +9,7 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Modification du mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/reserve", " Vos réservation", "/pages/allResa.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
